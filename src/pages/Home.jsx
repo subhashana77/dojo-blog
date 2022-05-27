9a0a2fb1038a4441ce983a3679ffbf8895a0ffbf
subhashana77 <106365502+subhashana77@users.noreply.github.com>
2022-05-27 16:50:23 +0530

    error handling
    catch the response data errors and handle the errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,35 @@ import BlogList from "../components/BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
             <div className="home">
+                {error && <div>{error}</div>}
+                {isPending && <div>Loading.....</div>}
                 {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
             </div>
         </div>
