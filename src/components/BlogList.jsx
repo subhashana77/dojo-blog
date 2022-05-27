5784116014dcb50d153a01a9cf33c6ea2cf275e0
subhashana77 <106365502+subhashana77@users.noreply.github.com>
2022-05-27 14:41:42 +0530

    filters
    code reusing and filter some data from object

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BlogList = ({blogs, title}) => {
+
+    // const blogs = props.blogs;
+    // console.log(props, blogs);
+
+    return (
+        <div className="blog-list">
+            <h3>{title}</h3>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import React, {useState} from "react";
+import BlogList from "../components/BlogList";
 
 const Home = () => {
+
     const [blogs, setBlog] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'dilshan', id: 1 },
         { title: 'Welcome party', body: 'lorem ipsum...', author: 'subhashana', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'rajapaksha', id: 3 },
     ]);
+
     return (
         <div className="home">
             <div className="home">
-                {blogs.map((blog) => (
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>Written by {blog.author}</p>
-                    </div>
-                ))}
+                <BlogList blogs={blogs} title="All Blogs"/>
+                <BlogList blogs={blogs.filter((blog) => blog.author === 'dilshan')} title="Dilshan's Blogs"/>
             </div>
         </div>
     );
