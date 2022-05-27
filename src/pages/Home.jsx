316fd365200a2a7b2b79dd9e523a9eeb82584fc2
subhashana77 <106365502+subhashana77@users.noreply.github.com>
2022-05-27 15:11:49 +0530

    useEffect
    get a knowledge about how to work use effect hook.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import BlogList from "../components/BlogList";
 
 const Home = () => {
@@ -9,15 +9,23 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'rajapaksha', id: 3 },
     ]);
 
+    const [name, setName] = useState('dilshan');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log("Use effect running");
+        console.log(name);
+    }, [name]);
+
     return (
         <div className="home">
             <div className="home">
                 <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+                <button onClick={()=> setName('subhashana')}>Change Name</button>
             </div>
         </div>
     );
