27d0283a6563849a93ae36f6a8d6930336e72615
subhashana77 <106365502+subhashana77@users.noreply.github.com>
2022-05-27 13:56:48 +0530

    outputting list
    use an object with user state hook to output the object details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,21 @@
 import React, {useState} from "react";
 
 const Home = () => {
-
-    const [name, setName] = useState("Dilshan");
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('Subhashana');
-        setAge(30);
-    }
-
+    const [blogs, setBlog] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'dilshan', id: 1 },
+        { title: 'Welcome party', body: 'lorem ipsum...', author: 'subhashana', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'rajapaksha', id: 3 },
+    ]);
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
+            <div className="home">
+                {blogs.map((blog) => (
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
